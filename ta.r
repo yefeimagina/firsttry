30f16a663532ffe6af4949a08117cf93cf1fe4d0
Your NameMagina <email@example.com251873053@qq.com>
2017-09-07 22:38:29 +0800

    change ta.r in yf

--- ta.r
+++ ta.r
@@ -4,3 +4,4 @@ library(rvest)
 url = "http://www.imdb.com/search/title?%20count=100&release_date=2016,2016&title_type=feature"
 a = read_html(url)
 y <- html_nodes(a,'.ratings-bar+ .text-muted')
+z = 1
